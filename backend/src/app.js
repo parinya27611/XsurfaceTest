bb909f0c3a3891c806dbe365cc15b6cfd5a76fb5
Joe <parinya27611@gamil.com>
2025-12-10 22:33:33 +0700

    Create uploads folder

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,17 +1,26 @@
 import express from "express";
 import cors from "cors";
+import fs from "fs";
+import path from "path";
 import dotenv from "dotenv";
 dotenv.config();
 
 import { connectDB } from "./config/db.js";
 import productRoutes from "./routes/product.routes.js";
 
+const uploadsPath = path.join(process.cwd(), "uploads");
+
+if (!fs.existsSync(uploadsPath)) {
+  fs.mkdirSync(uploadsPath);
+  console.log("📂 uploads folder created");
+}
+
 const app = express();
 
 app.use(cors({ origin: process.env.CORS_ORIGIN }));
 app.use(express.json());
 
-app.use("/uploads", express.static("./uploads"));
+app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 
 connectDB();
 
